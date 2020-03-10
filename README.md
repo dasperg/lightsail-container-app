97acef8cb6dae1ab23ba59cf04a0b7a7d5030207
dasper <dasperg@gmail.com>
2020-03-10 13:43:02 +0100

    Add portainer installation to launch-script.sh Add process how to create Lightsail instance

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Lightsail container app
 
 This is simple tutorial how to launch Lightsail instance with dockerized app.
+
+1. Create AWS account and go to [Lightsail console](https://lightsail.aws.amazon.com/ls/webapp/home/instances)
+2. Create new instance
+  * Select instance location
+  * Select platform `Linux/Unix`
+  * Select blueprint `OS Only` -> `Ubuntu`
+  * In optional section `Add launch script` and paste [launch-script.sh](launch-script.sh) content.
+  * Other fields are not needed for this simple tutorial
+  * Push the button `Create Instance`
--- launch-script.sh
+++ launch-script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#Author: https://github.com/mikegcoleman/todo
+
 # install latest version of docker the lazy way
 curl -sSL https://get.docker.com | sh
 
 # make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
+usermod -aG docker $USER
 
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# And this is my part:
+
+# create portainer volume
+docker volume create portainer_data
+
+# start portainer container
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
